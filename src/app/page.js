5499a4a6d08495a432f41791c87d04b0f1f23d45
muzamil hashmi <muzamilhashmi220077@gmail.com>
2023-11-18 03:34:46 +0500

    remaining

--- src/app/page.js
+++ src/app/page.js
@@ -2,6 +2,7 @@ import Card from '@/components/Card/Card'
 import Footer from '@/components/Footer/Footer'
 import Header from '@/components/Header/Header'
 import Hero from '@/components/Hero/Hero'
+import TopRated from '@/components/TopRated/TopRated'
 import Image from 'next/image'
 
 export default function Home() {
@@ -10,6 +11,7 @@ export default function Home() {
      
       <Hero />
       <Card/>
+      <TopRated/>
       {/* <h1 className='text-3xl'>Home Page</h1> */}
 
     </main>
---
+++ src/components/TopRated/TopRated.js
@@ -0,0 +1,100 @@
+import React from 'react'
+import './Toprated..css'
+const TopRated = () => {
+    return (
+        <>
+
+
+<div className='my-10 bg-cover bg-center min-h-screen' style={{ backgroundImage: "url('/assets/10052.jpg')" }}>
+
+
+            <div className='text-center py-10'> 
+
+            <h1>KNOW ABOUT US</h1>
+                <h1 className='text-5xl my-6'>
+                    TOP RATED STEAMERS
+                </h1>
+
+                <button className="bg-transparent border hover:bg-green-500 mx-5 text-white font-bold py-2 px-4 rounded">
+                    Buy Hero
+                </button>
+                <button className="bg-transparent hover:bg-yellow-500 border  mx-5 text-white font-bold py-2 px-4 rounded ">
+                    Buy Hero
+                </button>
+
+            </div>
+
+<div className='w-full border-t border-gradient-t'></div>
+
+
+            <div className='flex justify-center items-center gap-5 my-5 '>
+
+
+
+                    <img src="assets/10014.png" alt="" />
+                    <img src="assets/10015.png" alt="" />
+                    <img src="assets/10016.png" alt="" />
+                    <img src="assets/10017.png" alt="" />
+                    <img src="assets/10018.png" alt="" />
+                    <img src="assets/10019.png" alt="" />
+                    <img src="assets/10020.png" alt="" />
+
+                </div>
+                <div className='w-full border-b border-gradient-b'></div>
+
+
+                <div className='flex gap-10 py-5 '>
+
+                    <img src="assets/10020.jpg" alt="" />
+
+               <div className='about__tab-wrap'>
+
+               <div className='flex gap-6'>
+
+<div className='w-96'>
+    <h1 className='text-3xl font-bold'> Human Game</h1>
+    <p className='text-yellow-400 mt-3 font-bold text-2xl'>RATE 50%</p>
+    <p className='mt-8'>Lorem ipsum dolor sit amet, consteur adipiscing Duis elementum sollicitudin is yaugue euismods Nulla ullamcorper. Morbi pharetra tellus miolslis, tincidunt massa venenatis.</p>
+</div>
+<div>
+    <ul>
+        <li className='flex items-center gap-3 bg-gradient-to-r from-gray-900 via-transparent to-black rounded-md p-4'>
+            <img src="/assets/10026.png" alt="" />
+            Chichi Dragon Ball
+
+        </li>
+        <li className='flex items-center gap-3 my-3 bg-gradient-to-r from-gray-900 via-transparent to-black rounded-md p-4'>
+            <img src="/assets/10027.png" alt="" />
+            Space Babe Night
+
+        </li>
+        <li className='flex items-center gap-3 bg-gradient-to-r from-gray-900 via-transparent to-black rounded-md p-4'>
+            <img src="/assets/10028.png" alt="" />
+            Dragon Ball
+
+        </li>
+
+    </ul>
+
+</div>
+
+</div>
+<div className='flex gap-5 mt-3'>
+
+
+    <p className='bg-gray-700  bg-opacity-40 text-center p-6 w-48'>DRAGON BALL</p>
+    <p className='bg-gray-700  bg-opacity-40 text-center p-6 w-48'>NFT MARKET</p>
+    <p className='bg-gray-700  bg-opacity-40 text-center p-6 w-48'>SUPPORT</p>
+
+    </div> 
+    
+    
+                  </div>
+                  
+                </div>
+
+            </div>
+        </>)
+}
+
+export default TopRated
